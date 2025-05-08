69f124dc92b4546d397697c1406c1fc3a3117f53
alvinbuye-256 <alvinbuye@gmail.com>
2025-05-08 15:34:51 +0300

    Added the ScopePractice js file

---
+++ SampleFolder/Scope/ScopePractice.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+{
+    let NumberLet = 10;
+    NumberLet = 9;
+}
+
+
+{
+    var NumberVar = 20;
+}
+
+{
+    const PiConst = 3.14;
+  
+}
+
+console.log(NumberLet);
+//console.log(NumberVar);
+//console.log(PiConst);
